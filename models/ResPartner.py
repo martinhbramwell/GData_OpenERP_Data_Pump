a9ea60416123fad5c5394b012b47353e4d66b21b
Martin H. Bramwell <martinhbramwell@gmail.com>
2013-03-31 18:43:35 +0000

    New class with carnal knowledge of the res.partner model
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
    
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	new file:   models/ResPartner.py

---
+++ models/ResPartner.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2013-03-19
+
+@author: Martin H. Bramwell
+'''
+from OErpModel import OErpModel
+
+OPENERP_MODULE_NAME = 'res.partner'
+
+class ResPartner(OErpModel):
+
+    def __init__(self):
+        super(ResPartner, self).__init__()
+        
+        self.methods = {
+              'chkTask': self.chkTask
+            , 'load': self.load
+        }
+
+
+    def process(self, wrksht, rowTask):
+        super(ResPartner, self).process(wrksht, rowTask)
+        
+        for idx, aMethod in enumerate(self.methodNames):
+
+            if super(ResPartner, self).todo(idx):
+                print '    #{} Doing "{}" now.'.format(idx+1, aMethod)
+                super(ResPartner, self).starting(idx)
+
+
+            	self.methods[aMethod](self.parameters)
+
+
+                super(ResPartner, self).finished(idx)
+                print '__'
+            else:
+                print '    #{} Skipping "{}"!'.format(idx+1, aMethod)
+
+            pass
+
+
+    def chkTask(self, parms):
+        print 'Task check for key "docs_key" found : "' + parms['docs_key'] + '"!'
+        print 'Task check for key "docs_sheet" found : "' + parms['docs_sheet'] + '"!'
+
+    def load(self, parms):
+        print 'Calling parent to load to "{}".'.format(OPENERP_MODULE_NAME)
+
+        data = super(ResPartner, self).load(parms, OPENERP_MODULE_NAME)
+        print 'Done in ResPartner!'
+        
