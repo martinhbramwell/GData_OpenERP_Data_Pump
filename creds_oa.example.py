dda18480b843313e8633053760d7d25cb6bb7c1c
Martin H. J. Bramwell <martinhbramwell@gmail.com>
2014-01-08 06:13:01 -0500

    Make its layout match the output of gspread/tests/get_google_oauth2_creds.py 	modified:   creds_oa.example.py

--- creds_oa.example.py
+++ creds_oa.example.py
@@ -1,12 +1,18 @@
+refresh_token = '1/Ev_pCoT**************************loWIxAco'
+client_secret = 'GYJ_q**********JNH0ppT'
+client_id = '142317537518-las**************************0hh8.apps.googleusercontent.com'
+#
 key_ring = {}
 key_ring['grant_type'] = 'refresh_token'
-key_ring['refresh_token'] = '1/Ev_pCoT**************************loWIxAco'
-key_ring['client_secret'] = 'GYJ_q**********JNH0ppT'
-key_ring['client_id'] = '142317537518-las**************************0hh8.apps.googleusercontent.com'
-
+key_ring['refresh_token'] = refresh_token
+key_ring['client_secret'] = client_secret
+key_ring['client_id'] = client_id
+#
+access_token = 'ya29.1.AADtN_UTVx_AQIk_hVJ9g69Sj4kiWrl_AP02M2ZFQx3d_Gxc_03nASw32VEedJfmcCQhBg'
+#
 credentials = {
       'cred_type': 'oauth'
     , 'key_ring': key_ring
-    , 'access_token': 'ya29.1.AADtN_V-955GxfYosVNVSOQ4mRrG1pfmIVMQ7RmLU3D9EFtrZ5QsGH4IZGwEP0vXecXkjw'
+    , 'access_token': access_token
 }
 
