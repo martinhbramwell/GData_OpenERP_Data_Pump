f4cafe9342d7c79d22c3ee25b715f18c98de5278
Martin H.J. Bramwell <martinhbramwell@gmail.com>
2014-03-10 09:41:19 -0400

    pep8 cleanup 	modified:   openerp_utils.py

--- openerp_utils.py
+++ openerp_utils.py
@@ -6,30 +6,41 @@ Created on 2013-11-25
 @author: Martin H. Bramwell
 '''
 
-import oerplib, sys
+import oerplib
+import sys
 import socket
 
 from models.OErpModel import OErpModel
 
+
 class OpenERP(object):
 
     def __init__(self, credentials):
-        print "Getting connection to {} for {}".format(credentials['db_name'], credentials['user_id'])
-	try:
-	    oerp = oerplib.OERP(server=credentials['host_name'], protocol='xmlrpc', port=credentials['host_port'])
-	    OErpModel.openErpConnection['super'] = oerp
-	    if credentials['db_name'] in oerp.db.list():
-	        db_connect(credentials['user_id'], credentials['user_pwd'], credentials['db_name'])
-	    else:
-	        print "There is no database called : {}".format(credentials['db_name'])
-	except socket.gaierror:
-	    sys.exit("Is this the correct URL : {}".format(credentials['host_name']))
-	except socket.error:
-	    sys.exit("Is this the correct port number : {}".format(credentials['host_port']))
+        db = credentials['db_name']
+        user_id = credentials['user_id']
+        host_name = credentials['host_name']
+        host_port = credentials['host_port']
+        user_pwd = credentials['user_pwd']
+
+        print "Getting connection to {} for {}".format(db, user_id)
+        try:
+            oerp = oerplib.OERP(
+                server=host_name, protocol='xmlrpc', port=host_port)
+            OErpModel.openErpConnection['super'] = oerp
+            if db in oerp.db.list():
+                db_connect(user_id, user_pwd, db)
+            else:
+                print "There is no database called : {}".format(db)
+        except socket.gaierror:
+            sys.exit(
+                "Is this the correct URL : {}".format(host_name))
+        except socket.error:
+            sys.exit(
+                "Is this the correct port number : {}".format(host_port))
+
 
 def db_connect(usr, pwd, db):
     oerp = OErpModel.openErpConnection['super']
     user = oerp.login(user=usr, database=db, passwd=pwd)
     OErpModel.openErpConnection['admin'] = user
     # print " - - {} - - ".format(user)
-
