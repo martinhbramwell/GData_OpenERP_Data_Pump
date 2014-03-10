df0c0e194bab33ef2a2bb84cb009d7ef79d926ba
Martin H.J. Bramwell <martinhbramwell@gmail.com>
2014-03-10 09:27:39 -0400

    pep8 cleanup 	modified:   manage_arguments.py

--- manage_arguments.py
+++ manage_arguments.py
@@ -9,31 +9,37 @@ Created on 2013-03-19
 import argparse
 PROG = "gDataTools.py"
 
+desc = 'A very simple tool for feeding Google Spreadsheet data into the '
+desc += 'XMLRPC channel of OpenERP V7. Please read about it here:\n'
+desc += 'http://bit.ly/1fiRd5e'
+
+msg_c = "to drop and create new credentials"
+msg_k = "The identity key of a Google Spreadsheets workbook."
+msg_r = "Row in Tasks sheet at which to start processing."
+
+
 def get():
 
-    desc = 'A very simple tool for feeding Google Spreadsheet data into the XMLRPC channel of OpenERP V7. Please read about it here:\n'
-    desc += 'https://github.com/martinhbramwell/GData_OpenERP_Data_Pump/blob/master/README.md'
-    
     usage = "usage: {} [options] arg".format(PROG)
-    parser = argparse.ArgumentParser(description=desc, prog = PROG)
-    
+    parser = argparse.ArgumentParser(description=desc, prog=PROG)
+
     # Required
     choose = parser.add_mutually_exclusive_group(required=True)
-    choose.add_argument("-c", "--clean", action='store_true', help = "to drop and create new credentials")
-    choose.add_argument('-k', '--workbook_key', help = 'The identity key of a Google Spreadsheets workbook.')
+    choose.add_argument(
+        "-c", "--clean", action='store_true', help=msg_c)
+    choose.add_argument(
+        '-k', '--workbook_key', help=msg_k)
 
     # Optional
-    parser.add_argument("-r", "--start_row", default = 4, dest="start_row"
-                      , help = "Row in Tasks sheet at which to start processing.")
-    
+    parser.add_argument(
+        "-r", "--start_row", default=4, dest="start_row", help=msg_r)
+
     return parser.parse_args()
-    
+
 if __name__ == '__main__':
-    
+
     args = get()
-    
+
     print args.clean
     print args.workbook_key
     print args.start_row
-
-
